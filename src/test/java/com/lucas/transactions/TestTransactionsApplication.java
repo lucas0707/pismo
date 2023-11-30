cfc5353c2aab992571db482cc9da6bcbeb0761be
Lucas Machado <lucasfl.machado@gmail.com>
2023-11-30 19:29:49 -0300

    initial commit

---
+++ src/test/java/com/lucas/transactions/TestTransactionsApplication.java
@@ -0,0 +1,23 @@
+package com.lucas.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestTransactionsApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(TransactionsApplication::main).with(TestTransactionsApplication.class).run(args);
+	}
+
+}
